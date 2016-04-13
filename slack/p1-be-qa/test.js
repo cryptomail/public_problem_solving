d1668c8647a9ea01a574e218cead0f155dfd9e26
Joshua Teitelbaum <joshuat@gmail.com>
2016-04-12 19:29:57 -0700

    building my vignette

--- slack/p1-be-qa/test.js
+++ slack/p1-be-qa/test.js
@@ -1,10 +1,35 @@
+
+/*
+Joshua Teitelbaum 4/12/2016
+Testing the Slack FILE API
+*/
+
 var assert = require('assert');
 var Promise = require('promise');
 var request = require('request');
-var fs = require('fs');
 
+/*
+Secret key never stored in source code, and only is accessible through configuration files not in any RCS
+*/
 var secretToken = process.env.SLACK_SECRET_TOKEN;
-var util = require('./util.js')(secretToken);
+
+
+/*
+Switchable environment qa.slack.com/api, devint.slack.com/api, or default slack.com/api/
+*/
+var baseURL = process.env.SLACK_API_BASEURL;
+
+
+
+/*
+If the base URL is not set assume we're testing PROD!
+!YOLO! :)
+*/
+if(!baseURL) {
+	baseURL = "https://slack.com/api/";
+}
+var util = require('./util.js')(baseURL, secretToken);
+
 
 describe('Test suite to test Slack 3 API Endpoints: files.upload, files.list, files.delete', function() {
 
@@ -15,8 +40,40 @@ Library setup and initialization:
 /*
 Never store keys in source code!  Only in configuration where access can/should be limited!
 */
+	describe('Library initialization for testing the file suite', function() {
+
+		it('Has tokens initialized', function(done) { 
+			if(!secretToken) {
+				assert(1==2,'could not get initialize secret token not defined!');
+			}
+			done();
+		});
+
+		it('Can load channels', function(done) {
+			util.getChannels().then( function success(channels) {
+				
+				assert(channels);
+				channels =
+				done();
+			},
+			function error(e) {
+				util.errorlog('Met error!',e);
+				assert(1==2,'Could not resolve channels');
+				done();
+			});
+		});
+		it('Can load find general channel', function(done) {
+			util.getChannel('general').then( function success(channel) {
+				
+				assert(channel);
+				done();
+			},
+			function error(e) {
+				util.errorlog('Met error!',e);
+				assert(1==2,'Could not resolve channel general');
+				done();
+			});
+		});
+	});
 	
-	if(!secretToken) {
-		assert(1==2,'could not get initialize secret token not defined!');
-	}
 });
--- slack/p1-be-qa/util.js
+++ slack/p1-be-qa/util.js
@@ -1,7 +1,22 @@
 
+var Promise = require('promise');
+var request = require('request');
+
+/*
+Global functions area for the util module
+*/
+
 function getErrorObject(){
+/*
+  Very silly but useful hack to extract the time and place of the error for easy peasy line and location or error tracking :)
+*/
     try { throw Error('') } catch(err) { return err; }
 }
+
+
+/*
+Extracting what we need from an error at the time of error assertion / emition
+*/
 
 function lineNumberAndFile() {
   var err = getErrorObject();
@@ -12,10 +27,18 @@ function lineNumberAndFile() {
   return caller_line;
 }
 
-module.exports = function(secretToken) {
+/*
+Main module here
+*/
+module.exports = function(baseURL, secretToken) {
   return {
   	secretToken: secretToken,
-  	
+    baseURL: baseURL,
+    channels: undefined,
+
+    /*
+      Use the console but emit error / warn / debug candy
+    */
   	errorlog: function() {
       var d = new Date();
       console.log(d, '*****ERROR' + lineNumberAndFile(), arguments)
@@ -24,6 +47,103 @@ module.exports = function(secretToken) {
       var d = new Date();
 
       console.log(d, '*****DEBUG' + lineNumberAndFile(), arguments)
+    },
+
+    /*
+    Issue a simple GET request on slack api: Object.Command
+    Assumes baseURL is here, along with the secret token.
+    Extra safety employed on construction of URL to ensure no hanky panky
+    */
+    issueSimpleGETRequest: function(object, command) {
+      var self = this;
+      var p = new Promise(function(resolve,reject) {
+
+        var URL = self.baseURL + object + '.' + command + '?' + 'token=' + encodeURIComponent(self.secretToken);
+
+        /*
+        Snap the main request
+        */
+        request.get(URL,function (err, res, body) {
+          if(err) {
+            reject(err);
+            return;
+          }
+          if(res.statusCode !== 200) {
+          
+            reject('Something bad happened at URL ' + URL);
+            return;
+          }
+                
+          var res = JSON.parse(body);
+
+          resolve(res);
+
+        });
+      });
+
+      return p;
+      
+      
+    },
+
+    /*
+    getChannel:
+    will employ getChannels which caches all the channels available
+    Linear search :_( eh cardinality < 1024?!  One thing to test in the next test library I make :)
+    */
+    getChannel: function(channelName) {
+      var self = this;
+      var p = new Promise(function(resolve,reject) {
+
+        self.getChannels(false).then(function success(channels) {
+
+          if(!channels || !channelName || channelName.length === 0) {
+            reject('getChannel failed: ' + channelName);
+            return;
+          }
+
+          var x,n;
+          n = channels.length;
+
+          for(x=0; x < n; x++) {
+            if(channels[x].name && channels[x].name.toLowerCase() === channelName.toLowerCase()) {
+              resolve(channels[x]);
+              return;
+            }
+          }
+          reject('Cannot find channel ' + channelName);
+        },
+        function error(e) {
+          reject(e);
+        })
+      });
+
+      return p;
+    },
+
+
+    getChannels: function(forceRefresh) {
+      var self = this;
+      var p = new Promise(function(resolve,reject) {
+        if((!self.channels) || forceRefresh) {
+          self.issueSimpleGETRequest('channels','list').then(
+          function success(data) {
+            if(!data.ok) {
+              reject('Data is not ok');
+              return;
+            }
+            self.channels = data.channels;
+            resolve(data.channels);
+          },
+          function error(e) {
+            reject(e);
+          });
+        } else {
+          resolve(self.channels);
+        }
+        
+      });
+      return p;
     }
   };
 };
