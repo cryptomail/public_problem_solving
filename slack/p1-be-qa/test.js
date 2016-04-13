99dbabd18ca67694fcf482a9b0e6b51999c5e2a8
Joshua Teitelbaum <joshuat@gmail.com>
2016-04-12 18:18:40 -0700

    outside of test library since it's super initialization

--- slack/p1-be-qa/test.js
+++ slack/p1-be-qa/test.js
@@ -3,6 +3,20 @@ var Promise = require('promise');
 var request = require('request');
 var fs = require('fs');
 
+var secretToken = process.env.SLACK_SECRET_TOKEN;
+var util = require('./util.js')(secretToken);
+
 describe('Test suite to test Slack 3 API Endpoints: files.upload, files.list, files.delete', function() {
 
+/*
+Library setup and initialization:
+*/
+
+/*
+Never store keys in source code!  Only in configuration where access can/should be limited!
+*/
+	
+	if(!secretToken) {
+		assert(1==2,'could not get initialize secret token not defined!');
+	}
 });
--- slack/p1-be-qa/util.js
+++ slack/p1-be-qa/util.js
@@ -1,3 +1,17 @@
+
+function getErrorObject(){
+    try { throw Error('') } catch(err) { return err; }
+}
+
+function lineNumberAndFile() {
+  var err = getErrorObject();
+  var caller_line = err.stack.split("\n")[5];
+  var index = caller_line.indexOf("at ");
+  var clean = caller_line.slice(index+2, caller_line.length);
+
+  return caller_line;
+}
+
 module.exports = function(secretToken) {
   return {
   	secretToken: secretToken,
